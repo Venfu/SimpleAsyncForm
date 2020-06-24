12e6ce0fe28209b45772c7e3bbe962a7281dc0f8
Venfu <venfu@me.com>
2020-06-24 19:33:43 +0200

    renommage variable

--- api/postAdherants.php
+++ api/postAdherants.php
@@ -2,20 +2,6 @@
 
 header('Content-Type: application/json');
 
-/**
- * activite
- * nom
- * prenom
- * email
- * date_naissance
- * telephone
- * adresse
- * code_postal
- * ville
- * pays
- * representant_legal
- */
-
 $resp;
 
 $servername = "localhost";
@@ -30,22 +16,22 @@ $PATTERN_TEL = "/^(0033|0|\+33)[1-9]([-. ]?[0-9]{2}){4}$/";
 $PATTERN_EMAIL = "/^([A-Z|a-z|0-9](\.|_){0,1})+[A-Z|a-z|0-9]\@([A-Z|a-z|0-9])+((\.){0,1}[A-Z|a-z|0-9]){2}\.[a-z]{2,3}$/i";
 $PATTERN_DATE = "/[0-9]{2}\/[0-9]{2}\/[0-9]{4}/";
 
-$regex['activite'] = ["regex" => $PATTERN_ALPHA_NUM, "require" => false];
-$regex['nom'] = ["regex" => $PATTERN_ALPHA, "require" => false];
-$regex['prenom'] = ["regex" => $PATTERN_ALPHA, "require" => false];
-$regex['email'] = ["regex" => $PATTERN_EMAIL, "require" => false];
-$regex['date_naissance'] = ["regex" => $PATTERN_DATE, "require" => false];
-$regex['telephone'] = ["regex" => $PATTERN_TEL, "require" => false];
-$regex['adresse'] = ["regex" => $PATTERN_ALPHA_NUM, "require" => false];
-$regex['code_postal'] = ["regex" => $PATTERN_CP, "require" => false];
-$regex['ville'] = ["regex" => $PATTERN_ALPHA, "require" => false];
-$regex['pays'] = ["regex" => $PATTERN_ALPHA, "require" => false];
-$regex['representant_legal'] = ["regex" => $PATTERN_ALPHA, "require" => false];
+$validators['activite'] = ["regex" => $PATTERN_ALPHA_NUM, "require" => false];
+$validators['nom'] = ["regex" => $PATTERN_ALPHA, "require" => false];
+$validators['prenom'] = ["regex" => $PATTERN_ALPHA, "require" => false];
+$validators['email'] = ["regex" => $PATTERN_EMAIL, "require" => false];
+$validators['date_naissance'] = ["regex" => $PATTERN_DATE, "require" => false];
+$validators['telephone'] = ["regex" => $PATTERN_TEL, "require" => false];
+$validators['adresse'] = ["regex" => $PATTERN_ALPHA_NUM, "require" => false];
+$validators['code_postal'] = ["regex" => $PATTERN_CP, "require" => false];
+$validators['ville'] = ["regex" => $PATTERN_ALPHA, "require" => false];
+$validators['pays'] = ["regex" => $PATTERN_ALPHA, "require" => false];
+$validators['representant_legal'] = ["regex" => $PATTERN_ALPHA, "require" => false];
 
 // Check values
 foreach ($_POST as $k => $v) {
-  if ($regex[$k]["require"] && !$_POST[$k]) $resp['Error'][$k] = "Required";
-  if ($_POST[$k] && !preg_match($regex[$k]["regex"], $v)) $resp['Error'][$k] = "Invalid";
+  if ($validators[$k]["require"] && !$_POST[$k]) $resp['Error'][$k] = "Required";
+  if ($_POST[$k] && !preg_match($validators[$k]["regex"], $v)) $resp['Error'][$k] = "Invalid";
 }
 
 if ($resp['Error']) {
